5ed79934a095efa39b613882c18f49a0b56ba209
H.U.B. <kunstkr1tik@web.de>
2024-08-03 19:03:39 +0200

    finish exercise 07 temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const FAHRENHEIT_TO_CELSIUS_OFFSET = 32
+const FAHRENHEIT_TO_CELSIUS_CONVERSION_FACTOR = 5 / 9;
+
+function oneDecimalRepresentation(temperature) {
+  return Math.round(temperature * 10) / 10;
+}
+
+const convertToCelsius = function(degreeInFahrenheit) {
+  let offset = degreeInFahrenheit - FAHRENHEIT_TO_CELSIUS_OFFSET
+  let degreeInCelsius = offset * FAHRENHEIT_TO_CELSIUS_CONVERSION_FACTOR;
+  return oneDecimalRepresentation(degreeInCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeInCelsius) {
+  let degreeInFahrenheit = degreeInCelsius / FAHRENHEIT_TO_CELSIUS_CONVERSION_FACTOR;
+  degreeInFahrenheit += FAHRENHEIT_TO_CELSIUS_OFFSET;
+  return oneDecimalRepresentation(degreeInFahrenheit);
 };
 
 // Do not edit below this line
