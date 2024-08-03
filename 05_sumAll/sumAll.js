cc7a7967f39b4d9e3b5726e96c18b763d707e5a7
H.U.B. <kunstkr1tik@web.de>
2024-08-03 18:52:56 +0200

    finish exercise 05 sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(rangeStart, rangeEnd) {
+    if (typeof rangeStart != "number"
+        || typeof rangeEnd != "number"
+        || rangeStart < 0
+        || rangeEnd < 0
+        || Math.floor(rangeStart) !== rangeStart
+        || Math.floor(rangeEnd) !== rangeEnd
+     ) {
+        return "ERROR";
+     }
 
+     let min = Math.min(rangeStart, rangeEnd) - 1;
+     let max = Math.max(rangeStart, rangeEnd);
+
+     let totalSum = (max * (max + 1)) / 2;
+     let subTractableSum = (min * (min + 1)) / 2;
+     return totalSum - subTractableSum;
 };
 
 // Do not edit below this line
