a7500af98a37e87e9d278176b29fae077f492d81
H.U.B. <kunstkr1tik@web.de>
2024-08-03 18:40:34 +0200

    Finish Exercise 04 remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,29 @@
-const removeFromArray = function() {
+const AMOUNT_OF_ITEMS_TO_BE_REMOVED = 1;
+const INDEX_OF_ITEMS_TO_BE_REMOVED_START_LIST = 1;
+
+const isRemoved = function(list, index) {
+    if (index >= 0) {
+        list.splice(index, AMOUNT_OF_ITEMS_TO_BE_REMOVED);
+        return true;
+    }
+    return false;
+}
+
+const removeElementFromArray = function (list, removeObj) {
+    return list.findIndex((elem) => {
+        return elem === removeObj;
+    })
+}
+
+const removeFromArray = function(list) {
+    let copyOfList = structuredClone(list);
+    let removeObjList = Array.from(arguments).slice(INDEX_OF_ITEMS_TO_BE_REMOVED_START_LIST);
+    for (removeObj of removeObjList) {
+        while(isRemoved(copyOfList, removeElementFromArray(copyOfList, removeObj))) {
+            // Everything is done in condition
+        }
+    }
+    return copyOfList;
 };
 
 // Do not edit below this line
