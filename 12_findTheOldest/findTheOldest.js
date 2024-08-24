010c9e91a1e9260003e604ef763007b1a6135b70
H.U.B. <kunstkr1tik@web.de>
2024-08-24 22:49:25 +0200

    finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.slice().sort((a, b) => {
+        const currentDate = new Date().getFullYear();
+        let aBirth = a.yearOfBirth ?? currentDate;
+        let aDeath = a.yearOfDeath ?? currentDate;
 
+        let bBirth = b.yearOfBirth ?? currentDate;
+        let bDeath = b.yearOfDeath ?? currentDate;
+        
+        let ageA = aDeath - aBirth;
+        let ageB = bDeath - bBirth;
+        return ageB - ageA;
+    })[0];
 };
 
 // Do not edit below this line
