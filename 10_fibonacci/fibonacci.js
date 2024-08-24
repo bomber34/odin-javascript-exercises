55a42c54d5f871e131410a5f0476ce10b3e3787e
H.U.B. <kunstkr1tik@web.de>
2024-08-24 22:23:12 +0200

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
 
+    // simple but recursion might be problematic for large n ...
+    // good enough for this exercise I guess
+    return n < 2 ? parseInt(n) : fibonacci(n-1) + fibonacci(n-2);
 };
 
 // Do not edit below this line
