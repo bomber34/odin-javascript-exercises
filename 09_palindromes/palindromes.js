84cb22600e3c0aba0401b410a22ba0cd178bc091
H.U.B. <kunstkr1tik@web.de>
2024-08-24 22:13:38 +0200

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let letters = str.toLowerCase().split('').filter((letter) => RegExp(/^[a-zA-Z0-9]/,'u').test(letter))
+    const limit = Math.floor(letters.length / 2);
+    let rightSide = letters.length-1;
+    for (let leftSide = 0; leftSide <= limit; leftSide++) {
+        if (letters[leftSide] != letters[rightSide]) {
+            return false;
+        }
+        rightSide--;
+    }
+    return true;
 };
 
 // Do not edit below this line
