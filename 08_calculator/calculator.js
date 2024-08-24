8984961d8ab34b6f09e1b3f837987f875ecaeacc
H.U.B. <kunstkr1tik@web.de>
2024-08-24 21:59:09 +0200

    solved exericse 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(lhs, rhs) {
+  return lhs + rhs;
 };
 
-const subtract = function() {
-	
+const subtract = function(lhs, rhs) {
+	return lhs - rhs;
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+	return list.reduce((sum, val) => sum + val, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(list) {
+  return list.reduce((product, val) => product * val, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	return n > 1 ? n * factorial(n-1) : 1;
 };
 
 // Do not edit below this line
